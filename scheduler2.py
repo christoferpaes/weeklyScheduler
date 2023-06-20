3b3b91645e1d3aa5c36e914c0e53277ad1af98ee
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-20 13:39:21 -0700

    Update scheduler2.py

--- scheduler2.py
+++ scheduler2.py
@@ -14,57 +14,42 @@ class Department:
     def __init__(self, name):
         self.name = name
         self.budget = 0
+        self.employees = []
 
 
 class ScheduleApp:
     def __init__(self, root):
         self.root = root
-        self.employees = []
-        self.department = None
+        self.departments = []
+        self.current_department = None
 
         self.root.title("Employee Scheduler")
         self.root.geometry("300x200")
 
-        self.label = tk.Label(root, text="Employee Scheduler", font=("Arial", 20, "bold"), pady=10)
-        self.label.pack()
+        self.label = None
+        self.check_first_time()
 
-        self.retrieve_button = tk.Button(root, text="Retrieve Records", command=self.retrieve_records)
-        self.retrieve_button.pack()
-
-        self.new_button = tk.Button(root, text="Start from Scratch", command=self.start_from_scratch)
-        self.new_button.pack()
-
-    def retrieve_records(self):
+    def check_first_time(self):
         try:
-            with open("employees.json", "r") as file:
+            with open("departments.json", "r") as file:
                 data = json.load(file)
-                self.employees = [
-                    Employee(**employee_data) for employee_data in data["employees"]
-                ]
-                self.department = Department(data["department"]["name"])
-                self.department.budget = data["department"]["budget"]
+                if "departments" in data:
+                    self.departments = [
+                        Department(department_data["name"]) for department_data in data["departments"]
+                    ]
         except FileNotFoundError:
-            messagebox.showinfo("Retrieve Records", "No records found.")
-        else:
-            messagebox.showinfo("Retrieve Records", "Records retrieved successfully.")
+            pass
 
-    def start_from_scratch(self):
-        if self.department is None:
+        if not self.departments:
             self.collect_department_data()
         else:
-            response = messagebox.askyesno("Start from Scratch", "Starting from scratch will delete all existing records. Are you sure you want to continue?")
-            if response == tk.YES:
-                self.clear_widgets()
-                self.department = None
-                self.employees = []
-                self.collect_department_data()
-
-
+            self.show_department_layout()
 
     def collect_department_data(self):
         self.clear_widgets()
 
-        self.label.config(text="Enter Department Details")
+        self.label = tk.Label(self.root, text="Enter Department Details")
+        self.label.pack()
 
         self.department_name_label = tk.Label(self.root, text="Department Name:")
         self.department_name_label.pack()
@@ -83,20 +68,32 @@ class ScheduleApp:
         department_name = self.department_name_input.get()
         budget = float(self.budget_input.get())
 
-        self.department = Department(department_name)
-        self.department.budget = budget
+        department = Department(department_name)
+        department.budget = budget
+
+        self.departments.append(department)
 
         self.department_name_input.delete(0, tk.END)
         self.budget_input.delete(0, tk.END)
 
-        messagebox.showinfo("Department Added", "Department details added successfully.")
+        self.show_department_layout()
 
-        self.collect_employee_data()
-
-    def collect_employee_data(self):
+    def show_department_layout(self):
         self.clear_widgets()
 
-        self.label.config(text="Enter Employee Details")
+        self.label = tk.Label(self.root, text="Select Department")
+        self.label.pack()
+
+        self.department_dropdown = tk.OptionMenu(self.root, self.current_department, *self.departments, command=self.show_employee_layout)
+        self.department_dropdown.pack()
+
+    def show_employee_layout(self, department):
+        self.clear_widgets()
+
+        self.current_department = department
+
+        self.label = tk.Label(self.root, text="Enter Employee Details")
+        self.label.pack()
 
         self.name_label = tk.Label(self.root, text="Name:")
         self.name_label.pack()
@@ -124,7 +121,7 @@ class ScheduleApp:
         open_hours = self.open_hours_input.get()
 
         employee = Employee(name, hourly_pay, open_hours)
-        self.employees.append(employee)
+        self.current_department.employees.append(employee)
 
         self.name_input.delete(0, tk.END)
         self.hourly_pay_input.delete(0, tk.END)
@@ -132,64 +129,12 @@ class ScheduleApp:
 
         messagebox.showinfo("Employee Added", "Employee details added successfully.")
 
-        if len(self.employees) >= 2:
-            self.collect_budget_data()
-
-    def collect_budget_data(self):
-        self.clear_widgets()
-
-        self.label.config(text="Enter Budget")
-
-        self.budget_label = tk.Label(self.root, text="Budget:")
-        self.budget_label.pack()
-        self.budget_input = tk.Entry(self.root)
-        self.budget_input.pack()
-
-        self.submit_budget_button = tk.Button(self.root, text="Submit", command=self.generate_schedule)
-        self.submit_budget_button.pack()
-
-    def generate_schedule(self):
-        budget = float(self.budget_input.get())
-
-        schedule_data = {
-            "department": {"name": self.department.name, "budget": self.department.budget},
-            "employees": [
-                {"name": employee.name, "hourly_pay": employee.hourly_pay, "open_hours": employee.open_hours.split(" - ")}
-                for employee in self.employees
-            ],
-            "budget": budget,
-        }
-
-        with open("employees.json", "w") as file:
-            json.dump(schedule_data, file, indent=4)
-
-        messagebox.showinfo("Schedule Generated", "Schedule generated successfully.")
-
     def clear_widgets(self):
         for widget in self.root.winfo_children():
             widget.destroy()
 
     def run(self):
-        self.root.protocol("WM_DELETE_WINDOW", self.save_records)
         self.root.mainloop()
-
-    def save_records(self):
-        if self.department and self.employees:
-            budget = float(self.budget_input.get()) if self.budget_input.get() else 0
-
-            schedule_data = {
-                "department": {"name": self.department.name, "budget": self.department.budget},
-                "employees": [
-                    {"name": employee.name, "hourly_pay": employee.hourly_pay, "open_hours": employee.open_hours.split(" - ")}
-                    for employee in self.employees
-                ],
-                "budget": budget,
-            }
-
-            with open("employees.json", "w") as file:
-                json.dump(schedule_data, file, indent=4)
-
-        self.root.destroy()
 
 
 if __name__ == "__main__":
